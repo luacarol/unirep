310118a28d3419f93deb05a8cdf61354928fde02
Luana Caroliny <luana.oliveira@autaza.com>
2023-05-17 08:11:48 -0300

    Iniciando o models

--- unirep_app/models.py
+++ unirep_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Republic(models.Model):
+    TYPES_GENRES = [
+        ("MA", "Male"),
+        ("FE", "Feminine"),
+        ("MI", "Mixed"),
+    ]
+
+    name = models.CharField(max_length=200)
+    cellphone = models.CharField(max_length=11)
+    address = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    cuty = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    gender = models.CharField(max_length=2, choices=TYPES_GENRES)
+    num_vacancies = models.IntegerField()
+    created_in = models.DateField()
