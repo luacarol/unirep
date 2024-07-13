c8adbfcfaaba8900767964583638c2154652752d
Luana Caroliny <luana.oliveira@autaza.com>
2024-07-13 19:06:23 -0300

    typography

--- frontend_unirep/src/components/Header/index.js
+++ frontend_unirep/src/components/Header/index.js
@@ -4,13 +4,13 @@ const Header = () => {
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
-                <label className={styles.label}>Home</label>
-                <label className={styles.label}>Repúblicas</label>
-                <label className={styles.label}>Itens à pagar</label>
+                <label className={`${styles.label} minor-subtitle`}>Home</label>
+                <label className={`${styles.label} minor-subtitle`}>Repúblicas</label>
+                <label className={`${styles.label} minor-subtitle`}>Itens à pagar</label>
             </nav>
 
             <div className={styles.login}>
-                <label className={styles.label}>Nome do usuário</label>
+                <label className={`${styles.label} minor-subtitle`}>Nome do usuário</label>
                 <button>Sair</button>
             </div>
         </header>
--- frontend_unirep/src/index.js
+++ frontend_unirep/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './styles/colors.css';
+import './assets/styles/colors.css';
+import './assets/styles/typography.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
