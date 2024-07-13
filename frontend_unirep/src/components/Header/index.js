958cf8ab39e7c27d65df7c127f96016c5a482f91
Luana Caroliny <luana.oliveira@autaza.com>
2024-07-13 13:57:51 -0300

    style header

--- frontend_unirep/src/components/Header/index.js
+++ frontend_unirep/src/components/Header/index.js
@@ -1,16 +1,16 @@
-import style from './style.modules.css'
+import styles from './style.module.css'
 
 const Header = () => {
     return (
-        <header className={style.header}>
-            <nav className={style.nav}>
-                <label>Home</label>
-                <label>Repúblicas</label>
-                <label>Itens à pagar</label>
+        <header className={styles.header}>
+            <nav className={styles.nav}>
+                <label className={styles.label}>Home</label>
+                <label className={styles.label}>Repúblicas</label>
+                <label className={styles.label}>Itens à pagar</label>
             </nav>
 
-            <div className={style.login}>
-                <label>Nome do usuário</label>
+            <div className={styles.login}>
+                <label className={styles.label}>Nome do usuário</label>
                 <button>Sair</button>
             </div>
         </header>
