d178e57b4885a2ec92e936a0f4334c5a8592cff4
Luana Caroliny <luana.oliveira@autaza.com>
2024-07-13 09:35:16 -0300

    testing connecting frontend with backend

--- frontend_unirep/src/App.js
+++ frontend_unirep/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [republics, setRepublics] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/republics/')
+      .then(response => {
+        setRepublics(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Republics</h1>
+      <ul>
+        {republics.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
