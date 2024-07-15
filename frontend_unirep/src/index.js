a55d0713130cf41ecfd4b97c97eb931733e8932f
Luana Caroliny <luana.oliveira@autaza.com>
2024-07-15 19:14:35 -0300

    margin bottom

--- frontend_unirep/src/index.js
+++ frontend_unirep/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './assets/styles/reset.css';
-import './index.css';
 import './assets/styles/colors.css';
 import './assets/styles/typography.css';
-  
+import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
