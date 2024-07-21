b157b5dcb6ae5355b563564de7e25c1a5036c2b1
Luana Caroliny <luana.oliveira@autaza.com>
2024-07-21 20:07:04 -0300

    using navegate

--- frontend_unirep/src/components/Header/index.js
+++ frontend_unirep/src/components/Header/index.js
@@ -1,14 +1,29 @@
 import Button from '../Button';
 import styles from './style.module.css'
 import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    const handleHome = () => {
+        navigate('/');
+    }
+
+    const handleRepublics = () => {
+        navigate('/republics')
+    }
+
+    const handleItensToPay = () => {
+        
+    }
+
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
-                <label className={`${styles.label} minor-subtitle`}>Home</label>
-                <label className={`${styles.label} minor-subtitle`}>Repúblicas</label>
-                <label className={`${styles.label} minor-subtitle`}>Itens à pagar</label>
+                <label className={`${styles.label} minor-subtitle`} onClick={handleHome}>Home</label>
+                <label className={`${styles.label} minor-subtitle`} onClick={handleRepublics}>Repúblicas</label>
+                <label className={`${styles.label} minor-subtitle`} onClick={handleItensToPay}>Itens à pagar</label>
             </nav>
 
             <div className={styles.login}>
