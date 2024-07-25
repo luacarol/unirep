d39c81dc2d39a8c6e008d0afe55714f9e2c034ab
Luana Caroliny <luana.oliveira@autaza.com>
2024-07-24 23:09:10 -0300

    select label behavior

--- frontend_unirep/src/components/pages/Login/index.js
+++ frontend_unirep/src/components/pages/Login/index.js
@@ -2,23 +2,42 @@ import styles from './style.module.css';
 import Input from '../../Input/index';
 import Button from '../../Button/index';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Login = () => {
     const navigate = useNavigate();
 
+    const [selectedLabel, setSelectedLabel] = useState('loginSelected')
+
     const handleToEnterButton = () => {
         navigate('/home')
+    }
+
+    const handleLoginLabel = () => {
+        setSelectedLabel('loginSelected')
+    }
+
+    const handleRegisterLabel = () => {
+        setSelectedLabel('registerSelected')
     }
 
     return (
         <div className={styles.container}>
 
             <div className={styles.loginContainer}>
-                
+
                 <div className={styles.loginOrRegister}>
 
-                    <h2 className={`${styles.loginLabel} bigger-subtitle`}>Login</h2>
-                    <h2 className={`bigger-subtitle`}>Cadastrar</h2>
+                    <h2
+                        className={`${styles.loginLabel} ${selectedLabel === 'loginSelected' ? styles.selectedLabel : ''} bigger-subtitle`}
+                        onClick={handleLoginLabel}>
+                        Login
+                    </h2>
+                    <h2
+                        className={`${selectedLabel === 'registerSelected' ? styles.selectedLabel : ''} bigger-subtitle`}
+                        onClick={handleRegisterLabel}>
+                        Cadastrar
+                    </h2>
 
                 </div>
 
