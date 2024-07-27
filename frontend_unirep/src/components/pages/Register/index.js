cf36c65e9c6d39f9e21a9fede2ba808b67a5fd86
Luana Caroliny <luana.oliveira@autaza.com>
2024-07-26 22:21:30 -0300

    change chosenForm content

--- frontend_unirep/src/components/pages/Register/index.js
+++ frontend_unirep/src/components/pages/Register/index.js
@@ -79,9 +79,19 @@ const Register = () => {
                     <div className={styles.line}></div>
 
                     <div className={styles.chosenForm}>
-                        <Input className={`${styles.item}`} variant='labelInput' label='Nome completo' placeholder='Luana Caroliny Pedroso de Oliveira' />
-                        <Input className={`${styles.item}`} variant='labelInput' label='Idade' placeholder='23 anos' />
-                        <Input className={`${styles.item}`} variant='labelInput' label='Whatsapp' placeholder='(12) 982173929' />
+                        {registerAsSelectedInput === 'memberSelected' ? (
+                            <>
+                                <Input className={`${styles.item}`} variant='labelInput' label='Nome completo' placeholder='Luana Caroliny Pedroso de Oliveira' />
+                                <Input className={`${styles.item}`} variant='labelInput' label='Idade' placeholder='23 anos' />
+                                <Input className={`${styles.item}`} variant='labelInput' label='Whatsapp' placeholder='(12) 982173929' />
+                            </>
+                        ) : (
+                            <>
+                                <Input className={`${styles.item}`} variant='labelInput' label='Nome completo' placeholder='Luana Caroliny Pedroso de Oliveira' />
+                                <Input className={`${styles.item}`} variant='labelInput' label='Whatsapp' placeholder='(12) 982173929' />
+                                <Input className={`${styles.item}`} variant='labelInput' label='CEP do Imóvel' placeholder='12228005' />
+                            </>
+                        )}
                     </div>
                 </div>
 
