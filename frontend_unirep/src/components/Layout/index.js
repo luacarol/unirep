109d65c1c094ac96ad0c477c8224c954ea016e71
Luana Caroliny <luana.oliveira@autaza.com>
2024-07-31 20:49:06 -0300

    adj toast and scrollbar

--- frontend_unirep/src/components/Layout/index.js
+++ frontend_unirep/src/components/Layout/index.js
@@ -1,15 +1,16 @@
 import Header from '../Header/index';
 import Footer from '../Footer/index';
+import styles from './style.module.css';
 
 const Layout = ({ children }) => {
     return (
-        <>
+        <div className={styles.layout}>
             <Header />
-            <div className='body'>
+            <div className={styles.body}>
                 {children}
             </div>
             <Footer />
-        </>
+        </div>
     )
 }
 
