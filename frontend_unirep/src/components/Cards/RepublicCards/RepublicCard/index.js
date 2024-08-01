b838f67d7091651adc7f6aabad03ac9399620d3a
Luana Caroliny <luana.oliveira@autaza.com>
2024-07-31 21:30:17 -0300

    creating iconLabel

--- frontend_unirep/src/components/Cards/RepublicCards/RepublicCard/index.js
+++ frontend_unirep/src/components/Cards/RepublicCards/RepublicCard/index.js
@@ -1,5 +1,7 @@
 import styles from './style.module.css';
 import republicImg from '../../../../assets/images/republic.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faNewspaper, faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
 
 const RepublicCard = ({ onClick }) => {
     return (
@@ -12,12 +14,14 @@ const RepublicCard = ({ onClick }) => {
             <div className={styles.republicInfo}>
                 <label className={`legend ${styles.republicNameLabel}`}>Nome da república</label>
                 
-                <div className={styles.numMembers}>
+                <div className={`${styles.numMembers} ${styles.iconLabel}`}>
+                    <FontAwesomeIcon icon={faPeopleGroup} className={styles.icon}/>
                     <label className={`legend`}>Quantidade de membros</label>
                 </div>
 
-                <div className={styles.description}>
-                    <label className={`legend ${styles.descriptionLabel}`}>Descrição breve de como é ou qual é a essência da república</label>
+                <div className={`${styles.description} ${styles.iconLabel}`}>
+                    <FontAwesomeIcon icon={faNewspaper} className={styles.icon}/>
+                    <label className={`small-text ${styles.descriptionLabel}`}>Descrição breve de como é ou qual é a essência da república</label>
                 </div>
 
                 <label className={`legend ${styles.republicValue}`}>R$ 400,00</label>
