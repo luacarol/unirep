425499d7fb317c1ae90719bc56ef1f46cd42953c
Luana Oliveira <luana.oliveira@autaza.com>
2024-12-16 23:38:50 -0300

    Styling the header component

---
+++ unirep_frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <h1 className="title">Unirep</h1>
+            <nav className={styles.nav}>
+                <ul>
+                    <li><a className="section" href="/">Login</a></li>
+                    <li><a className="section" href="/about">Cadastrar-se</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
--- unirep_frontend/src/components/Layout/Layout.jsx
+++ unirep_frontend/src/components/Layout/Layout.jsx
@@ -1,18 +1,10 @@
+import Header from "../Header/Header";
 import styles from "./Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.layout}>
-      {/* Header */}
-      <header className={styles.header}>
-        <h1>My Website</h1>
-        <nav>
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-          </ul>
-        </nav>
-      </header>
+      <Header/>
 
       {/* Main Content */}
       <main className={styles.main}>
