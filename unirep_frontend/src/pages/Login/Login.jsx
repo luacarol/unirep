81966a93aea562d2b87c298203b0c47153e2d367
Luana Oliveira <luana.oliveira@autaza.com>
2024-12-17 22:47:25 -0300

    Login screen created

--- unirep_frontend/src/pages/Login/Login.jsx
+++ unirep_frontend/src/pages/Login/Login.jsx
@@ -3,7 +3,29 @@ import styles from "./Login.module.css";
 const Login = () => {
     return (
         <div className={styles.login_container}>
+            <h1 className="title">Login</h1>
 
+            <div className={styles.login_content}>
+
+                <div className={styles.inputs}>
+                    <div className={styles["label-input_container"]}>
+                        <label className={`section ${styles.label}`}>Email</label>
+                        <input className={`section ${styles.input}`} type="text" />
+                    </div>
+
+                    <div className={styles["label-input_container"]}>
+                        <label className={`section ${styles.label}`}>Password</label>
+                        <input className={`section ${styles.input}`} type="password" />
+                    </div>
+                </div>
+
+                <button className={styles["icon-label_button"]}><label className={`section ${styles.label}`}>Entrar</label></button>
+
+                <div className={styles.register_section}>
+                    <a className={`label link ${styles.link}`} href="/register">Cadastrar-se</a>
+                </div>
+
+            </div>
         </div>
     )
 }
