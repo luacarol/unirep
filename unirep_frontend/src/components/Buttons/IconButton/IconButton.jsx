1a81ccd8413a49ea49fcd00b0aab82359258808d
Luana Oliveira <luana.oliveira@autaza.com>
2025-01-03 16:28:25 -0300

    Setting size property on icon button

--- unirep_frontend/src/components/Buttons/IconButton/IconButton.jsx
+++ unirep_frontend/src/components/Buttons/IconButton/IconButton.jsx
@@ -1,12 +1,15 @@
-import styles from "./IconButton.module.css"; 
+import styles from "./IconButton.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const IconButton = ({ icon }) => {
+const IconButton = ({ icon, size = "medium" }) => {
+    // size: small, medium, large
+    const sizeClass = styles[size] || styles.medium; // Define uma classe padrão caso `size` seja inválido
+
     return (
-        <button className={styles.icon_button}>
+        <button className={`${styles.icon_button} ${sizeClass}`}>
             <FontAwesomeIcon icon={icon} />
         </button>
-    )
-}
+    );
+};
 
 export default IconButton;
--- unirep_frontend/src/components/Header/Header.jsx
+++ unirep_frontend/src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
+import IconButton from "../Buttons/IconButton/IconButton";
 import styles from "./Header.module.css";
 import { useNavigate } from "react-router-dom";
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     const navigate = useNavigate();
+    const ifLoggedIn = true;
 
     const handleTitleClick = () => {
         navigate("/");
@@ -15,6 +18,12 @@ const Header = () => {
                 <ul>
                     <li><a className="section" href="/login">Login</a></li>
                     <li><a className="section" href="/register">Cadastrar-se</a></li>
+
+                    {ifLoggedIn == true && <li><a className="section" href="/"><IconButton icon={faBars} size="large"/></a></li>}
+                    
+                    {ifLoggedIn == true && <li><a className="section" href="/">
+                        <img src="" className={styles.profile_img} />
+                    </a></li>}
                 </ul>
             </nav>
         </header>
