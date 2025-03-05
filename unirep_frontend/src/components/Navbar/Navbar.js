eabac1a570fecdd6243ba9249565f7c78d0218cd
Luana Caroliny <lucarolp.deoli@gmail.com>
2025-03-05 12:32:34 -0300

    feat: creating navbar

---
+++ unirep_frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css"; // Importando os estilos
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h2>UniRep</h2>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/register">Cadastrar-se</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
