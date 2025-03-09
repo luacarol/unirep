a49a07ecf756fbeb1e1a41418ad47fb80458bf2a
Luana Caroliny <luanacaroliny07@gmail.com>
2025-03-09 15:29:35 -0300

    feat: putting notMember in bill card

--- unirep_frontend/src/components/BillCard/BillCard.js
+++ unirep_frontend/src/components/BillCard/BillCard.js
@@ -6,6 +6,7 @@ import Modal from "../Modal/Modal";
 
 const BillCard = ({ }) => {
     let status = "Pago";
+    let notMember = true;  // Alterei o valor para false, de acordo com seu exemplo
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const handleOpenModal = () => setIsModalOpen(true);
@@ -19,26 +20,32 @@ const BillCard = ({ }) => {
                 <label className="legend">Nome da Conta</label>
                 <label className="smaller-text">R$ 150,00</label>
 
-                {status == "Pagar" && <ButtonLine text="Pagar" />}
-                {status == "Pago" && <label className="legend paid">Pago</label>}
+                {notMember === false && (
+                    <>
+                        {status === "Pagar" && <ButtonLine text="Pagar" />}
+                        {status === "Pago" && <label className="legend paid">Pago</label>}
+                    </>
+                )}
             </div>
 
             <div className="info-icon" onClick={handleOpenModal}>
                 <i className="fa-solid fa-circle-info"></i>
             </div>
 
-            {isModalOpen && (<Modal
-                handleOpenModal={handleOpenModal}
-                handleCloseModal={handleCloseModal}
-                infos={
-                    <div className="label-value">
-                        <label className="legend">Explicação da conta:</label>
-                        <label className="smaller-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.    </label>
-                    </div>
-                }
-            />)}
+            {isModalOpen && (
+                <Modal
+                    handleOpenModal={handleOpenModal}
+                    handleCloseModal={handleCloseModal}
+                    infos={
+                        <div className="label-value">
+                            <label className="legend">Explicação da conta:</label>
+                            <label className="smaller-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. </label>
+                        </div>
+                    }
+                />
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default BillCard;
--- unirep_frontend/src/pages/RepublicDetails/Bills/Bills.js
+++ unirep_frontend/src/pages/RepublicDetails/Bills/Bills.js
@@ -3,25 +3,6 @@ import "./Bills.css";
 import BillCard from "../../../components/BillCard/BillCard";
 
 const Bills = () => {
-    // const [bills, setBills] = useState([]);
-
-    // Definindo as contas apenas uma vez na montagem do componente
-    // useEffect(() => {
-    //     setBills([
-    //         { id: 1, name: "Conta de Luz", amount: 120, dueDate: "10/03/2025", status: "Não Pago" },
-    //         { id: 2, name: "Conta de Água", amount: 80, dueDate: "15/03/2025", status: "Pago" },
-    //         { id: 3, name: "Conta de Internet", amount: 100, dueDate: "20/03/2025", status: "Não Pago" },
-    //     ]);
-    // }, []); // O array vazio [] garante que isso rode apenas na primeira renderização.
-
-    // const handlePayBill = (id) => {
-    //     setBills((prevBills) =>
-    //         prevBills.map((bill) =>
-    //             bill.id === id ? { ...bill, status: "Pago" } : bill
-    //         )
-    //     );
-    // };
-
     return (
         <section className="bills-section">
             <h3 className="bigger-subtitle">Itens à Pagar</h3>
