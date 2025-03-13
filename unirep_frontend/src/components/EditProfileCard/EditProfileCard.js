0a8edc60157bb7c622aea7741ee6cbdba041b247
Luana Caroliny <luanacaroliny07@gmail.com>
2025-03-13 18:47:28 -0300

    refact: refactoring info and style in home to user

--- unirep_frontend/src/components/EditProfileCard/EditProfileCard.js
+++ unirep_frontend/src/components/EditProfileCard/EditProfileCard.js
@@ -1,25 +1,33 @@
+import { useState } from "react";
 import "./EditProfileCard.css";
 import userImage from "../../assets/images/user.svg";
 import ButtonLine from "../Buttons/ButtonLine/ButtonLine";
 import { Link } from "react-router-dom";
 
 const EditProfileCard = () => {
+    const [user, setUser] = useState({
+        name: "Isabele Oliveira",
+        age: 24,
+        phone: "(31) 98765-4321",
+        hobby: "Gosta de tocar violão",
+        dislikes: "Não gosta de bagunça"
+    });
+
     return (
         <div className="edit-profile-card">
             <div className="profile-image"><img src={userImage} alt="User profile"/></div>
             
             <div className="basic-infos">
-                <label className="minor-subtitle">Nome do Usuário</label>
+                <label className="minor-subtitle">{user.name}</label>
 
-                <label className="text-commom">24 anos</label>
+                <label className="text-commom">{user.age} anos</label>
 
-                <label className="text-commom">(12) 982173929</label>
+                <label className="text-commom">{user.phone}</label>
             </div>
 
             <div className="description-button">
                 <div className="description-info">
-                    <label className="text-commom">Descrição</label>
-                    <label className="smaller-text">Descrição curta do perfil do usuário falando da sua personalidade.</label>
+                    <label className="smaller-text">Sou {user.name} tenho {user.age} anos e {user.hobby} e {user.dislikes}</label>
                 </div>
 
                 <Link className="link" to="/edit-profile"><ButtonLine text="Editar meu perfil" /></Link>
--- unirep_frontend/src/components/UserRepublic/UserRepublic.js
+++ unirep_frontend/src/components/UserRepublic/UserRepublic.js
@@ -1,28 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UserRepublic.css";
-import republicImage from "../../assets/images/undraw_small_town_re_7mcn 1.svg"
+import republicImage from "../../assets/images/undraw_small_town_re_7mcn 1.svg";
 import ButtonLine from "../Buttons/ButtonLine/ButtonLine";
 import { Link } from "react-router-dom";
 
-const UserRepublic = ({ republic }) => {
+const UserRepublic = () => {
+    const [republic, setRepublic] = useState({
+        name: "República Aurora",
+        address: "Rua das Palmeiras, 123, Centro",
+        cityState: "Belo Horizonte, MG - 30100-000",
+        description: "Uma república acolhedora com um ambiente descontraído e organizado, perfeita para quem busca tranquilidade e boas amizades."
+    });
+
     return (
         <div className="user-republic-card">
             <div className="republic-image"><img src={republicImage} alt="Republic logo" /></div>
 
             <div className="basic-infos">
-
-                <label className="minor-subtitle">Nome da República</label>
-
-                <label className="text-commom">Endereço, Bairro.<br/>CEP, Cidade, Estado</label>
-
-                <label className="text-commom">(00) 00000-00</label>
-
+                <label className="minor-subtitle">{republic.name}</label>
+                <label className="text-commom">{republic.address}<br/>{republic.cityState}</label>
             </div>
 
             <div className="description-button">
                 <div className="description-info">
-                    <label className="text-commom">Descrição</label>
-                    <label className="smaller-text">Curta descrição da cultura e da vibe dentro da república, falando de suas características físicas e peculiares.</label>
+                    <label className="smaller-text">{republic.description}</label>
                 </div>
 
                 <Link className="link" to="/republic-details"><ButtonLine text="Ver detalhes"/></Link>
