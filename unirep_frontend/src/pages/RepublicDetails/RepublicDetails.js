4fda11b6cc432c693414fd42dbfc22bd200ffa24
Luana Caroliny <luanacaroliny07@gmail.com>
2025-03-13 18:27:53 -0300

    feat: creating modal confirmation

--- unirep_frontend/src/pages/RepublicDetails/RepublicDetails.js
+++ unirep_frontend/src/pages/RepublicDetails/RepublicDetails.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import ButtonIcon from "../../components/Buttons/ButtonIcon/ButtonIcon";
 import MemberCard from "../../components/MemberCard/MemberCard";
 import Bills from "./Bills/Bills";
 import "./RepublicDetails.css";
 import RepublicInfo from "./RepublicInfo/RepublicInfo";
+import Modal from "../../components/Modal/Modal";
 
 const mockMembers = [
     {
@@ -48,17 +50,44 @@ const mockMembers = [
 ];
 
 const RepublicDetails = () => {
+    const [isRequestModalOpen, setIsRequestModalOpen] = useState(false);
+    const [isInfoConfirmModal, setIsInfoConfirmModal] = useState(false);
+
+    const handleOpenRequestModal = () => setIsRequestModalOpen(true);
+    const handleCloseRequestModal = () => setIsRequestModalOpen(false);
+
+    const handleInfoConfirmModal = () => {
+        setIsInfoConfirmModal(true);
+        setTimeout(() => {
+            setIsInfoConfirmModal(false);
+            setIsRequestModalOpen(false);
+        }, 4000); // Fecha automaticamente após 4 segundos
+    };
+
+    const handleCloseInfoConfirmModal = () => {
+        setIsInfoConfirmModal(false);
+        setIsRequestModalOpen(false);
+    }
+
     let notMember = true;
 
     return (
         <div className="republic-details">
             <div className="title-button-section">
                 <h2 className="bigger-subtitle">🏠 Detalhes da Moradia!</h2>
-                {notMember == true && <div className="button"><ButtonIcon text="Solicitar Entrada" iconClass="fa-solid fa-door-open" /></div>}
+
+                {notMember && (
+                    <div className="button">
+                        <ButtonIcon
+                            text="Solicitar Entrada"
+                            iconClass="fa-solid fa-door-open"
+                            onClick={handleOpenRequestModal}
+                        />
+                    </div>
+                )}
             </div>
 
             <RepublicInfo />
-
             <Bills />
 
             <section className="section members-section">
@@ -71,6 +100,43 @@ const RepublicDetails = () => {
                 </div>
             </section>
 
+            {/* Modal de Solicitação de Entrada */}
+            {isRequestModalOpen && (
+                <Modal
+                    title="Solicitação de Entrada"
+                    handleOpenModal={handleOpenRequestModal}
+                    handleCloseModal={handleCloseRequestModal}
+                    infos={
+                        <div className="request-content-modal">
+                            <label className="text-commom">Você deseja solicitar entrada nesta república?</label><br />
+                            <label className="text-commom">Sua solicitação será enviada para análise.</label>
+
+                            <div className="yes-no-buttons">
+                                <ButtonIcon text="Confirmar" iconClass="fa-solid fa-check" onClick={handleInfoConfirmModal} />
+
+                                {/* <ButtonIcon text="Cancelar" iconClass="fa-solid fa-ban" /> */}
+                            </div>
+                        </div>
+                    }
+                />
+            )}
+
+            {/* Modal de Informação de Confirmação */}
+            {isInfoConfirmModal && (
+                <Modal
+                    title="Informação de Confirmação"
+                    handleOpenModal={handleInfoConfirmModal}
+                    handleCloseModal={handleCloseInfoConfirmModal}
+                    infos={
+                        <div className="info-confirm-content-modal">
+                            <label className="text-commom">Sua solicitação de entrada na República Beta foi enviada para análise.</label>
+                            <label className="text-commom">Aguarde a resposta do administrador</label>
+
+                            <ButtonIcon iconClass="fa-solid fa-circle-check" onlyIcon={true} onClick={handleCloseInfoConfirmModal} />
+                        </div>
+                    }
+                />
+            )}
         </div>
     );
 };
